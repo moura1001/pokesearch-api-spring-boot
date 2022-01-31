e7cad2b515b66212e16c02a82d9845a4bf575373
moura1001 <gmouraneto01@gmail.com>
2022-01-31 08:36:12 -0300

    primeiro commit com configuração inicial do projeto

---
+++ src/test/java/com/example/pokesearch/PokeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
